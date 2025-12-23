2c2952f2ef909ec4c1587e02007711e06bb99645
tanishenigma <tanish23enigma@gmail.com>
2025-12-24 00:12:16 +0530

    Added custom middleware & deleted password return in get request

--- Part-4/models/user.model.js
+++ Part-4/models/user.model.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String },
+  password: { type: String, minlength: 8 },
   blogs: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Blog",
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.password;
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
 });
 
