63c9b67b6d8ca1f60f45f92ddfde46b4d10f59a5
tanishenigma <tanish23enigma@gmail.com>
2025-12-24 00:16:51 +0530

    Added Passwword Hash

--- Part-4/controllers/user.controller.js
+++ Part-4/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 const getUser = async (req, res, next) => {
   try {
@@ -9,12 +10,19 @@ const getUser = async (req, res, next) => {
   }
 };
 const createUser = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (!password || password.length < 8) {
+    return res.status(400).json({
+      error: "Password must be at least 8 characters long",
+    });
+  }
   try {
-    const { username, email, password } = req.body;
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
     const newUser = new User({
       username,
       email,
-      password,
+      passwordHash,
     });
     const savedUser = await newUser.save();
     res.send(savedUser);
