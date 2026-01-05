6771708383b2be3076278f8c98261db6db49bfc6
tanishenigma <tanish23enigma@gmail.com>
2026-01-05 18:34:40 +0530

    added toggle view/hide details

--- Part-5/src/components/Blog.jsx
+++ Part-5/src/components/Blog.jsx
@@ -1,7 +1,54 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [view, setView] = useState(false);
+  console.log(view);
+  return (
+    <>
+      {!view && (
+        <div style={blogStyle}>
+          {blog.title + " "}{" "}
+          <button
+            onClick={() => {
+              setView(!view);
+            }}>
+            View
+          </button>
+        </div>
+      )}
+      {view && (
+        <div style={blogStyle}>
+          <div
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "4px",
+            }}>
+            <h3> {blog.title} </h3>
+            <button
+              onClick={() => {
+                setView(!view);
+              }}
+              style={{ height: "30px" }}>
+              hide
+            </button>
+          </div>
+          <div>Author: {blog.author}</div>
+          <div>
+            Likes: {blog.likes} <button>like</button>
+          </div>
+          <div> URL: {blog.url}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Blog;
