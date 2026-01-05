714c3fe7f925d1cbf454f3062471e5de97a538c9
tanishenigma <tanish23enigma@gmail.com>
2026-01-05 18:10:12 +0530

    toggle Create Blog Button

--- Part-5/src/components/BlogForm.jsx
+++ Part-5/src/components/BlogForm.jsx
@@ -19,50 +19,74 @@ const BlogForm = ({ setNewBlog }) => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+  const [create, setCreate] = useState(false);
+  setTimeout(() => {
+    setCreateMessage(null);
+  }, 10000);
   return (
     <div>
-      <p
-        style={{
-          color: "#027D02",
-          background: "#D3D3D3",
-          padding: "10px",
-          border: "5px solid #027D02",
-          borderRadius: "8px",
-        }}>
-        {createMessage}
-      </p>
-      <form onSubmit={createBlog}>
-        <label>Titile:</label>
-        <input
-          type="text"
-          name={title}
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <br />
-        <label>Author:</label>
-        <input
-          type="text"
-          name={author}
-          value={author}
-          onChange={(e) => {
-            setAuthor(e.target.value);
-          }}
-        />
-        <br />
-        <label>Url:</label>
-        <input
-          name={url}
-          value={url}
-          onChange={(e) => {
-            setUrl(e.target.value);
-          }}
-        />
-        <br />
-        <button>Submit</button>
-      </form>
+      {createMessage && (
+        <p
+          style={{
+            color: "#027D02",
+            background: "#D3D3D3",
+            padding: "10px",
+            border: "5px solid #027D02",
+            borderRadius: "8px",
+          }}>
+          {createMessage}
+        </p>
+      )}
+      {create && (
+        <button
+          onClick={() => {
+            setCreate(!create);
+          }}>
+          Create new blog
+        </button>
+      )}
+      {!create && (
+        <form onSubmit={createBlog}>
+          <label>Titile:</label>
+          <input
+            type="text"
+            name={title}
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <br />
+          <label>Author:</label>
+          <input
+            type="text"
+            name={author}
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+          />
+          <br />
+          <label>Url:</label>
+          <input
+            name={url}
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+          />
+          <br />
+          <button>Submit</button>
+        </form>
+      )}
+      {!create && (
+        <button
+          onClick={() => {
+            setCreate(!create);
+          }}>
+          Cancel
+        </button>
+      )}
     </div>
   );
 };
